1ee64673acc1c022a84fcd975e53aee36cef557c
gianluca-gradogna <gianluca.gradogna@mono-grid.com>
2023-10-11 21:34:58 +0200

    webflow-try

--- src/index.js
+++ src/index.js
@@ -1,166 +1,174 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import gsap from 'gsap'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { TextureLoader } from 'three';
-
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  // console.log('hello');
-  init3D();
-});
-
-// Init Function
-function init3D() {
-  // select container
-  const viewport = document.querySelector('[data-3d="c"]');
-  // console.log(viewport);
-
-/*------------------------------
-  Scene, Camera, Renderer
-  ------------------------------*/
-  const scene = new THREE.Scene();
-
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.z = 1;
-
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  viewport.appendChild(renderer.domElement);
-
-  /*------------------------------
-  Object
-  ------------------------------*/
-  const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
-  const material = new THREE.ShaderMaterial({
-    vertexShader: `
-      varying vec2 vUv;
-
-      void main() {
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-
-        vUv = uv;
-      }
-    `,
-    fragmentShader: ` 
-      varying vec2 vUv;
-
-      void main() {
-        float strength = distance(vUv, vec2(0.5));
-
-        gl_FragColor = vec4(strength, strength, strength, 1.0);
-      }
-    `
-  });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
 
 
-  /*------------------------------
-  Controls
-  ------------------------------*/
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.enabled = false
-  // controls.autoRotate = true;
-  // controls.enableDamping = true;
-  // controls.dampingFactor = 0.05;
+export default class Sketch {
+  constructor(options) {
+    this.scene = new THREE.Scene();
+
+    this.container = options.dom;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer = new THREE.WebGLRenderer( { 
+      antialias: true,
+      alpha: true,
+     } );
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setClearColor(0xeeeeee, 1); 
+
+    this.container.appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      this.width/this.height,
+      100,
+      2000
+    );
+
+    // var frustumSize = 10;
+    // var aspect = window.innerWidth / window.innerHeight;
+    // this.camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, -1000, 1000 );
+    this.camera.position.set(0, 0, 600);
+
+    this.camera.fov = 2*Math.atan( (this.height/2)/600)* (180/Math.PI);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.time = 0;
+
+    this.images = [...document.querySelectorAll('img')];
+
+    // Preload images
+    const preloadImages = new Promise((resolve, reject) => {
+      imagesLoaded(document.querySelectorAll("img"), { background: true }, resolve);
+    });
+
+    let allDone = [preloadImages]
 
 
-  /*------------------------------
-  Loop
-  ------------------------------*/
 
-  function animate() {
-    requestAnimationFrame(animate);
-    controls.update();
-
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+    Promise.all(allDone).then(()=>{
+      this.addImages();
+      this.setPosition();
+      this.addObjects();
+      this.resize();
+      this.render();
+      this.setupResize();
+      // this.settings();
+    })
+    this.isPlaying = true;
   }
 
-  animate();
+  settings() {
+    let that = this;
+    this.settings = {
+      progress: 0,
+    };
+    this.gui = new dat.GUI();
+    this.gui.add(this.settings, "progress", 0, 1, 0.01);
+  }
 
-  /*------------------------------
-  Resize
-  ------------------------------*/
-  function onWindowResize() {
-    camera.aspect = viewport.clientWidth / viewport.clientHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( viewport.clientWidth, viewport.clientHeight );
-    }
-    window.addEventListener( 'resize', onWindowResize, false );
-  
-   /*------------------------------
-  Mouse Move
-  ------------------------------*/
+  setupResize() {
+    window.addEventListener("resize", this.resize.bind(this));
+  }
 
-  function onMouseMove(e) {
-    const x = e.clientX
-    const y = e.clientY
-    
-    gsap.to(scene.rotation, {
-        y: gsap.utils.mapRange(0, viewport.clientWidth, .2, -.2, x),
-        x: gsap.utils.mapRange(0, viewport.clientHeight, .2, -.2, y)
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
+  }
+
+  addImages(){
+    this.imageStore = this.images.map(img=>{
+      let bounds = img.getBoundingClientRect()
+
+      let geometry = new THREE.PlaneGeometry(bounds.width,bounds.height,1,1);
+      let texture = new THREE.Texture(img);
+      texture.needsUpdate = true;
+      let material = new THREE.MeshBasicMaterial({
+        // color: 0xff0000,
+        map: texture
+      })
+      let mesh = new THREE.Mesh(geometry,material);
+
+      this.scene.add(mesh)
+
+      return {
+        img: img,
+        mesh: mesh,
+        top: bounds.top,
+        left: bounds.left,
+        width: bounds.width,
+        height: bounds.height
+      }
     })
+  }
+
+  setPosition(){
+    this.imageStore.forEach(o=>{
+      o.mesh.position.y = -o.top + this.height/2 - o.height/2;
+      o.mesh.position.x = o.left - this.width/2 + o.width/2;
+    })
+  }
+
+  addObjects() {
+    let that = this;
+    this.material = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: "#extension GL_OES_standard_derivatives : enable"
+      },
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: { value: 0 },
+        resolution: { value: new THREE.Vector4() },
+      },
+      wireframe: true,
+      // transparent: true,
+      vertexShader: `
+        varying vec2 vUv;
+        void main() {
+          vUv = uv;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+        }
+      `,
+      fragmentShader: ` 
+        varying vec2 vUv;
+        void main()	{
+          // vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
+          gl_FragColor = vec4(vUv,0.0,1.);
+        }
+      `
+    });
+
+    this.geometry = new THREE.PlaneGeometry(200, 400, 10, 10);
+
+    this.plane = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.plane);
+  }
+
+  stop() {
+    this.isPlaying = false;
+  }
+
+  play() {
+    if(!this.isPlaying){
+      this.render()
+      this.isPlaying = true;
     }
-    window.addEventListener('mousemove', onMouseMove)
+  }
+
+  render() {
+    if (!this.isPlaying) return;
+    this.time += 0.05;
+    this.material.uniforms.time.value = this.time;
+    requestAnimationFrame(this.render.bind(this));
+    this.renderer.render(this.scene, this.camera);
+  }
 }
 
-
-//   /*------------------------------
-//   Load 3d async
-//   ------------------------------*/
-//   const assets = load();
-//   assets.then((data) => {
-//     console.log(data, data.robot);
-
-//     data.robot.traverse((child) => {
-//       if (child.isMesh) {
-//         child.material = new THREE.MeshBasicMaterial();
-//         child.material.map = data.texture;
-//       }
-//     });
-
-//     data.robot.position.y = -1;
-//     scene.add(data.robot);
-//   });
-// }
-
-// /*------------------------------
-//   Loader Functions
-//   ------------------------------*/
-// async function load() {
-//   const robot = await loadModel(
-//     'https://uploads-ssl.webflow.com/64102f194260e3387db26189/64108688b73a86e15101dad4_robot.glb.txt'
-//   );
-
-//   const texture = await loadTexture(
-//     'https://uploads-ssl.webflow.com/64102f194260e3387db26189/6410867a42a4acda86412cc4_robot-texture.png'
-//   );
-
-//   return { robot, texture };
-// }
-
-// const textureLoader = new TextureLoader();
-// const modelLoader = new GLTFLoader();
-
-// function loadTexture(url) {
-//   return new Promise((resolve) => {
-//     textureLoader.load(url, (data) => {
-//       data.needsUpdate = true;
-//       data.flipY = false;
-
-//       resolve(data);
-//     });
-//   });
-// }
-
-// function loadModel(url, id) {
-//   return new Promise((resolve, reject) => {
-//     modelLoader.load(url, (gltf) => {
-//       const result = gltf.scene;
-//       resolve(result);
-//     });
-//   });
-// }
+new Sketch({
+  dom: document.getElementById("container")
+});
